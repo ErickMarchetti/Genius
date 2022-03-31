9a84ec180f4c1518ea582f928fc0ca5d27c3d275
Erick <erickgmarchetti@gmail.com>
2022-03-30 22:11:50 -0300

    estilizacao finalizada

--- funcionalidades.js
+++ funcionalidades.js
@@ -23,7 +23,9 @@ function piscaCores(arrayDeCores = sequenciaDeCoresGeradas) {
 
         if(contadorDoTemporizador === arrayDeCores.length) {
             clearInterval(temporizador)
-            rmOrAddEvent('add')
+            setTimeout(() => {
+                rmOrAddEvent('add')
+            }, 1200)
         }
 
     }, 1200)
@@ -39,9 +41,9 @@ function guardarCorClicada(event) {
 
     event.stopPropagation()
 
-    const botaoClicadoAtual = event.target.id
+    const idBotaoClicadoAtual = event.target.id
 
-    sequenciaDeCoresUsuario.push(botaoClicadoAtual)
+    sequenciaDeCoresUsuario.push(idBotaoClicadoAtual)
 
 }
 
@@ -72,11 +74,13 @@ function rmOrAddEvent(removerOuAdicionar) {
     if(removerOuAdicionar === 'rm') {
         botoesColoridos.forEach(item => item.removeEventListener('click', guardarCorClicada))
         botoesColoridos.forEach(item => item.removeEventListener('click', compararCorClicada))
+        botoesColoridos.forEach(item => item.classList.remove('botao'))
     }
 
     else if(removerOuAdicionar === 'add') {
         botoesColoridos.forEach(item => item.addEventListener('click', guardarCorClicada))
         botoesColoridos.forEach(item => item.addEventListener('click', compararCorClicada))
+        botoesColoridos.forEach(item => item.classList.add('botao'))
     }
 }
 
