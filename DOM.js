be0bdb3fae7fe48b10e360c3955edbbc27133c82
Jamily Lima <jamilylima70@gmail.com>
2022-03-31 08:28:55 -0300

    Ajustando elemento DOM

--- DOM.js
+++ DOM.js
@@ -1,20 +1,23 @@
 const principal = document.getElementById("principal")
-principal.innerHTML=`  <h1> Jogo Genius Estrela </h1>
-<section class="game" id="container">
-    <div class="cores top-left" id="verde"></div>
-    <div class="cores top-right" id="amarelo"></div>
-    <div class="cores bottom-left" id="azul"></div>
-    <div class="cores bottom-right" id="vermelho"></div>
+principal.innerHTML=`<h1>Genius Estrela</h1>
 
-   
-    <div class="centro">Iniciar</div>
- 
+<div class="container">
+    <section class="game" id="container">
+        <div class="cores top-left botao" id="verde"></div>
+        <div class="cores top-right botao" id="vermelho"></div>
+        <div class="cores bottom-left botao" id="azul"></div>
+        <div class="cores bottom-right botao" id="amarelo"></div>
     
-</section>
+        <div class="centro" id="centro">JOGAR NOVAMENTE</div>
+        
+    </section>
 
-<section class="score">
-    <p >Pontuação</p>
-    <p class="pontos">0</p>
-</section>
-
-<p class="frase">E aí vamos começar?</p>`
+    <div class="infos">
+        <section class="score">
+            <p>Pontuação</p>
+            <p>0</p>
+        </section>
+        
+        <p class="frase">E aí vamos começar?</p>
+    </div>
+</div>`
